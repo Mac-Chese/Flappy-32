5bec2e11242e09a79cb272b40dd7d803cb1b61f3
ronDjon <95055653+ronDjon@users.noreply.github.com>
2023-12-05 00:08:02 -0500

    Final commit of the flappy bird esp32 code

--- flappyesp32.ino
+++ flappyesp32.ino
@@ -1,45 +1,60 @@
+/**
+Authors: Matt Barber, Aaron Morris
+Date: 12/4/23
+Description: This is a flappy bird game made to run on an esp32 development board
+**/
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "pitches.h"
+#include <Preferences.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 #define BUTTON_PIN 2
-#define BUZZER_PIN 23 // ESP32 GPIO3 pin connected to Buzzer's pin
-#define BUZZER_2_PIN 0
+#define BUZZER_PIN 0
 #define POT_PIN 15
 
 const int birdStartingPosY = 16;
-const int gravityAmount = 1;
+const int gravityAmount = 3;
 const int jumpAmount = 18;
-const int pipeYMin = 8;
+const int pipeYMin = 15;
 const int pipeYMax = 48;
 const int birdX = 16;
 const int birdRad = 5;
+const int pipeRadius = 2;
+const int pipeWidth = 10;
+const int pipeHeight = 64; 
+const int pipeSeperator = 38;
+const int pipeNoise1 = 2500;
+const int pipeNoise2 = 3700;
+const int birdNoise = 620;
+const int deathNoise = 84;
+
 
 int birdY = birdStartingPosY;
-int lastBirdY = 0;
-int pipeX = 128;
+int pipeX = 120;
 int pipeY = random(pipeYMin, pipeYMax);
-int lastPipeX = 148;
-int lastPipeY = 32; 
 
-int pipeRadius = 2;
-int pipeWidth = 10;
-int pipeHeight = 64; 
-int pipeSeperator = 38;
+unsigned int currentScore = 0;
+unsigned int highScore = -1;
 
-int delayTime = -1;
-// notes in the melody:
-int melody[] = {
-  NOTE_A7,
-  NOTE_B2
-};
+bool gravityFlag = false;
+bool buttonLevel = false, lastButtonLevel = true;
+bool pastPipe;
+
+void moveBird(int xPosition, int yPosition);
+void movePipe(int pipeX, int pipeY);
+bool pipeHitDetection(int birdX, int birdY, int pipeX, int pipeY);
+void startMenu();
+void drawMenuScoreBox();
+void drawGameScoreBox();
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+Preferences preferences;
 
 void setup() {
   Serial.begin(115200);
@@ -50,70 +65,67 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
+
   delay(2000);
   display.clearDisplay();
   display.display(); 
   delay(100);
-  
- 
+
+  preferences.begin("flappy", false);
+
+  highScore = preferences.getUInt("highScore", 0);
+
+  preferences.end();
 }
 
 void loop() {
-  int potADCReading;
-  int gravityCounter = 0;
+  int delayTime;
+  int potADC;
   unsigned long startTime, currentTime;
-  bool buttonLevel = false, lastButtonLevel = true;
-  bool gravityFlag = false;
-  bool loser = true;
+  
+  bool loser = false;
   bool justLost = false;
-    
-  moveBird(birdX, birdY, birdRad, birdX, lastBirdY, birdRad);
-  movePipe(pipeX, pipeY, lastPipeX, lastPipeY);
+
+  display.clearDisplay();
+  moveBird(birdX, birdY);
+  movePipe(pipeX, pipeY);
+
   display.display();
 
   while (true) {
-    buttonLevel = digitalRead(BUTTON_PIN);
+    
     justLost = false;
+    pipeY = random(pipeYMin, pipeYMax);
 
-    if (lastButtonLevel == false && buttonLevel == true) {
-      loser = false;
-      
-      Serial.println("Starting game");
+    startMenu();
+    loser = false;
+    Serial.println("Starting Now");
 
-      // reset game
-      birdY = birdStartingPosY;
-      lastBirdY = 0;
-      pipeX = 128;
-      pipeY = random(pipeYMin, pipeYMax);
-      lastPipeX = 148;
-      lastPipeY = 32; 
-
-      display.clearDisplay();
-
-    }
+    display.clearDisplay();
 
     lastButtonLevel = buttonLevel;
 
     while (!loser)  {
       startTime = millis();
-      
-      potADCReading = analogRead(POT_PIN);
-      delayTime = map(potADCReading, 0, 4095, 0, 500);
-
-      currentTime = millis();
+      potADC = analogRead(POT_PIN);
+      delayTime = map(potADC, 0, 4095, 0, 500);
 
       do {
-
-        moveBird(birdX, birdY, birdRad, birdX, lastBirdY, birdRad);
-        movePipe(pipeX, pipeY, lastPipeX, lastPipeY);
-        lastBirdY = birdY;
-        lastPipeX = pipeX;
-        lastPipeY = pipeY;
+        display.clearDisplay();
+        drawGameScoreBox();
+        moveBird(birdX, birdY);
+        movePipe(pipeX, pipeY);
         display.display();
 
+        loser = pipeHitDetection(birdX, birdY, pipeX, pipeY);
+        justLost = loser;
+
+        // read the button
         buttonLevel = digitalRead(BUTTON_PIN);
 
-        if (lastButtonLevel == false && buttonLevel == true) {
+        // check if the button is going from low to high to indicate a jump
+        if (lastButtonLevel == false && buttonLevel == true && !justLost) {
+          // if we are at the celing then don't keep going otherwise go up more
           if ((birdY - jumpAmount) <= 2) {
             birdY = 3;
           }
@@ -121,166 +133,220 @@ void loop() {
             birdY -= jumpAmount;
           }
           
-          tone(BUZZER_PIN, melody[0], 20);
+          tone(BUZZER_PIN, birdNoise, 20);
 
         }
         lastButtonLevel = buttonLevel;
 
-        if (birdY < SCREEN_HEIGHT && gravityFlag) {
+        // gravity only subtracts from the height every other time
+        if (gravityFlag && !justLost) {
           birdY += gravityAmount;
+          gravityFlag = false;
           }
-        else if (birdY >= SCREEN_HEIGHT && gravityFlag) {
-          loser = true;
-          justLost = true;
-        } 
-
-        // how frequently gravity takes effect, every two cycles
-        if (!gravityFlag) {
-          if (gravityCounter < 2){
-            gravityCounter ++;
-          }
-          
-          else {
-            gravityFlag = true;
-            gravityCounter = 0;
-          }
+        else {
+          gravityFlag = true;
         }
 
         currentTime = millis();
 
-        if (hitDetection(birdX, birdY, pipeX, pipeY)) {
-          Serial.print(millis());
-          Serial.println(", hit detected");
-          loser = true;
-          justLost = true;
-        }
-        
       } while (((currentTime - startTime) < delayTime) && !loser);
 
-      if (pipeX > 0){
+      if (pipeX > 0) {
         pipeX -= 4;
       }
       else  {
-        pipeX = 128;
+        pipeX = 120;
         pipeY = random(pipeYMin, pipeYMax);
       }
     } 
 
     // after you lose you have to wait half a second to start again
     if (justLost) {
-      tone(BUZZER_2_PIN, melody[1], 500);
+      noTone(BUZZER_PIN);
+      tone(BUZZER_PIN, deathNoise, 500);
+      
+      // if the high score is new save it in flash
+      if (currentScore == highScore) {
+      preferences.begin("flappy", false);
+      preferences.putUInt("highScore", highScore);
+      preferences.end();
+      }
+
       delay(500);
     }
   }
 }
 
 
+void startMenu()  {
+    pipeX = 120;
+    birdY = 16;
+    currentScore = 0;
+    
+    while (true) {
+
+      display.clearDisplay();
+      drawMenuScoreBox();
+      moveBird(birdX, birdY);
+      movePipe(pipeX, pipeY);
+      display.display();
+  
+      
+      if (birdY > 48) {
+        birdY = 16;
+     
+        tone(BUZZER_PIN, birdNoise, 10);
+      }
+
+      if (gravityFlag) {
+          birdY += gravityAmount;
+          gravityFlag = false;
+          }
+      else {
+          gravityFlag = true;
+      }
+      buttonLevel = digitalRead(BUTTON_PIN);
+
+      // check if the button is pressed to start game
+      if (lastButtonLevel == false && buttonLevel == true) {
+        birdY -= jumpAmount;
+        break;
+      }
+
+      lastButtonLevel = buttonLevel;
+
+    }
+
+}
+
+
 // returns true if hit is detected, false otherwise
-bool hitDetection(int birdX, int birdY, int pipeX, int pipeY){
+bool pipeHitDetection(int birdX, int birdY, int pipeX, int pipeY){
   int hitBoxLowY, hitBoxHighY;
-  int hitBoxRight;
+  int hitBoxRight, hitBoxLeft;
   int hitBoxMargin = (pipeSeperator / 2);
   int pipeLeft, pipeRight;
   bool birdHit = false;
   
-  pipeLeft = pipeX;
-  pipeRight = pipeX + pipeWidth;
+  pipeLeft = pipeX - (pipeWidth / 2);
+  pipeRight = pipeX + (pipeWidth / 2);
   hitBoxRight = birdX + birdRad;
+  hitBoxLeft = birdX - birdRad;
+ 
+  // checks if we are hitting the bottom
+  if (birdY >= SCREEN_HEIGHT){
+    birdHit = true;
+  }
 
-  for (int i = 0; i < ((birdRad + 1) * 2); i++) {
+  // checks if the left or right side of the birds hitbox could hit the pipe and if it could then it checks if it did
+  else if (((hitBoxRight > pipeLeft) && (hitBoxRight < pipeRight)) || ((hitBoxLeft > pipeLeft) && (hitBoxLeft < pipeRight))) {
+    
+    if ((birdY + birdRad) > (pipeY + hitBoxMargin)){
+      birdHit = true;
+    }
 
-    if (((hitBoxRight - i) >= pipeLeft) && ((hitBoxRight - i) <= pipeRight)) {
-      
-      if (birdY > (pipeY + hitBoxMargin)){
-        birdHit = true;
-        break;
-      }
-
-      else if (birdY < (pipeY - hitBoxMargin)){
-        birdHit = true;
-        break;
-      }
+    else if ((birdY - birdRad) < (pipeY - hitBoxMargin)){
+      birdHit = true;
     }
   }
+
+  else if (pipeRight <= 5 && !pastPipe)  {
+      pastPipe = true;
+      
+      currentScore++;
+      if (currentScore > highScore) {
+        highScore = currentScore;
+      }
+
+      Serial.print("Current Score:\t");
+      Serial.println(currentScore);
+      Serial.print("High Score:\t");
+      Serial.println(highScore);
+
+      tone(BUZZER_PIN, pipeNoise1, 10);
+      tone(BUZZER_PIN, pipeNoise2, 5);
+    }
+  
+  else if (pipeRight > 5) {
+    pastPipe = false;
+  }
+
 
   return birdHit;
 
 }
 
-void movePipe(int pipeX, int pipeY, int lastPipeX, int lastPipeY) { 
-  int topX, topY, botX, botY;
-  int lastTopX, lastTopY, lastBotX, lastBotY;
-
-  lastTopX = lastPipeX - (pipeWidth / 2);
-  lastTopY = lastPipeY - (pipeHeight + ((pipeSeperator - 1)) / 2);
-  lastBotX = lastPipeX - (pipeWidth / 2);
-  lastBotY = lastPipeY + ((pipeSeperator - 1) / 2);
-
-  topX = pipeX - (pipeWidth / 2);
-  topY = pipeY - (pipeHeight + ((pipeSeperator - 1)) / 2);
-  botX = pipeX - (pipeWidth / 2);
-  botY = pipeY + ((pipeSeperator - 1) / 2);
-
-  display.fillRoundRect(lastTopX, lastTopY, pipeWidth, pipeHeight, pipeRadius, BLACK);
-  display.fillRoundRect(lastBotX, lastBotY, pipeWidth, pipeHeight, pipeRadius, BLACK);
-
-  // draw new ones
-  display.fillRoundRect(topX, topY, pipeWidth, pipeHeight, pipeRadius, WHITE);
-  display.fillRoundRect(botX, botY, pipeWidth, pipeHeight, pipeRadius, WHITE);
+void drawGameScoreBox() {
+  // score box in the upper left
+  int scoreCursorX = 96;
+  int scoreCursorY = 56;
+  
+  display.setCursor(scoreCursorX, scoreCursorY);
+  display.setTextColor(WHITE);
+  display.print("CS ");
+  display.print(currentScore);
 
 }
 
 
-void moveBird(int xPosition, int yPosition, int birdRadius, int lastXPosition, int lastYPosition, int lastBirdRadius) {
+void drawMenuScoreBox() {
+  // score box in the upper left
+  int scoreCursorX = 24;
+  int scoreCursorY = 56;
   
-  //draw bird here
-  int circleX0, circleY0, circleRadius;
+  display.setCursor(scoreCursorX, scoreCursorY);
+  display.setTextColor(WHITE);
+  display.print("LS ");
+  display.print(currentScore);
+  display.print("  ");
+  display.print("HS ");
+  display.print(highScore);
+
+}
+
+void movePipe(int pipeX, int pipeY) { 
+  int topX, topY, botY;
+
+  topX = pipeX - (pipeWidth / 2);
+  topY = pipeY - (pipeHeight + ((pipeSeperator - 1)) / 2);
+  botY = pipeY + ((pipeSeperator - 1) / 2);
+
+  // draw new ones
+  display.fillRoundRect(topX, topY, pipeWidth, pipeHeight, pipeRadius, WHITE);
+  display.fillRoundRect(topX, botY, pipeWidth, pipeHeight, pipeRadius, WHITE);
+
+}
+
+
+void moveBird(int xPosition, int yPosition) {
+  int circleX0, circleY0;
   int triangleX0, triangleY0, triangleX1, triangleY1, triangleX2, triangleY2;
   int birdEyeX0, birdEyeY0, birdEyeRadius;
 
-  int lastCircleX0, lastCircleY0, lastCircleRadius;
-  int lastTriangleX0, lastTriangleY0, lastTriangleX1, lastTriangleY1, lastTriangleX2, lastTriangleY2;
-
   circleY0 = yPosition;
   circleX0 = xPosition;
-  circleRadius = birdRadius;
 
-  triangleX0 = xPosition + ( 1.75  * circleRadius) ;
+  triangleX0 = xPosition + ( 1.75  * birdRad) ;
   triangleY0 = yPosition;
-  triangleX1 = xPosition + (0.75 * circleRadius);
-  triangleY1 = yPosition - (0.75 * circleRadius);
-  triangleX2 = xPosition + (0.75 * circleRadius);
-  triangleY2 = yPosition + (0.75 * circleRadius);
+  triangleX1 = xPosition + (0.75 * birdRad);
+  triangleY1 = yPosition - (0.75 * birdRad);
+  triangleX2 = xPosition + (0.75 * birdRad);
+  triangleY2 = yPosition + (0.75 * birdRad);
 
   birdEyeX0 = xPosition;
-  birdEyeY0 = yPosition - (birdRadius / 2.33333333);
-  birdEyeRadius = birdRadius / 6;
+  birdEyeY0 = yPosition - (birdRad / 2.33333333);
+  birdEyeRadius = birdRad / 6;
   
   if (birdEyeRadius == 0) {
     birdEyeRadius = 1;
   }
 
-  lastCircleY0 = lastYPosition;
-  lastCircleX0 = lastXPosition;
-  lastCircleRadius = lastBirdRadius;
-
-  lastTriangleX0 = lastXPosition + ( 1.75  * lastCircleRadius) ;
-  lastTriangleY0 = lastYPosition;
-  lastTriangleX1 = lastXPosition + (0.75 * lastCircleRadius);
-  lastTriangleY1 = lastYPosition - (0.75 * lastCircleRadius);
-  lastTriangleX2 = lastXPosition + (0.75 * lastCircleRadius);
-  lastTriangleY2 = lastYPosition + (0.75 * lastCircleRadius);
-
-  display.fillCircle(lastCircleX0, lastCircleY0, lastBirdRadius + 1, BLACK); // hitbox
-  display.fillTriangle(lastTriangleX0, lastTriangleY0, lastTriangleX1, lastTriangleY1, lastTriangleX2, lastTriangleY2, BLACK); // hitbox
-
-  display.drawCircle(circleX0, circleY0, birdRadius + 1, BLACK); // outline
+  display.drawCircle(circleX0, circleY0, birdRad + 1, BLACK); // outline
 
   display.fillTriangle(triangleX0, triangleY0, triangleX1, triangleY1, triangleX2, triangleY2, WHITE); // beak
-  display.fillCircle(circleX0, circleY0, circleRadius, WHITE); // body
+  display.fillCircle(circleX0, circleY0, birdRad, WHITE); // body
   display.fillCircle(birdEyeX0, birdEyeY0, birdEyeRadius, BLACK); // eye
   display.drawTriangle(triangleX0, triangleY0, triangleX1, triangleY1, triangleX2, triangleY2, BLACK); // beak outline
 
   
-
 }
